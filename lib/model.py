cba6468e2776ad71a7d6c8d0489dbf0b4c0596fb
joshua chivile <joshuachivile@gmail.com>
2023-12-14 16:07:59 +0300

    Added Employee Model

--- lib/model.py
+++ lib/model.py
@@ -26,3 +26,14 @@ class Category(Base):
     #relationship
     products = relationship("Product",backref="category")
     
+# Employee Model
+class Employee(Base):
+    __tablename__ = "employees"
+    id = Column(Integer(),primary_key=True)
+    employee_firstname = Column(String(50),nullable=False)
+    employee_lastname = Column(String(50),nullable=False)
+    employee_email = Column(String(50),nullable=False)
+
+    # relationship
+    products = relationship("Product",backref="employee")
+
