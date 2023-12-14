a1ca589f3cb12bf25164c78c22d21c306821f081
joshua chivile <joshuachivile@gmail.com>
2023-12-14 13:53:25 +0300

    Empty init of migration

---
+++ lib/migrations/versions/d1125d4fab3f_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: d1125d4fab3f
+Revises: 
+Create Date: 2023-12-14 13:50:28.466632
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd1125d4fab3f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
