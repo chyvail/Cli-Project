34bed6d239eba42fb060a082be03bcb1c305f75e
joshua chivile <joshuachivile@gmail.com>
2023-12-20 19:24:56 +0300

    removed unused import

--- lib/model.py
+++ lib/model.py
@@ -1,5 +1,5 @@
 # imports
-from sqlalchemy import create_engine, Integer, Column, String, ForeignKey, desc
+from sqlalchemy import create_engine, Integer, Column, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
